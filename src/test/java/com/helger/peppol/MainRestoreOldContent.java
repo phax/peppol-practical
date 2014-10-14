f77089196c303b7d5fbc2990c4a0e61c1dd2f513
Philip Helger <philip@helger.com>
2014-10-14 09:25:30 +0200

    Changed package names

--- src/test/java/com/helger/peppol/MainRestoreOldContent.java
+++ src/test/java/com/helger/peppol/MainRestoreOldContent.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.webappdemo;
+package com.helger.peppol;
 
 import java.io.File;
 import java.util.Map;
--- src/test/java/com/helger/peppol/jetty/JettyStopPEPPOL.java
+++ src/test/java/com/helger/peppol/jetty/JettyStopPEPPOL.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.webappdemo.jetty;
+package com.helger.peppol.jetty;
 
 import java.io.IOException;
 import java.io.OutputStream;
