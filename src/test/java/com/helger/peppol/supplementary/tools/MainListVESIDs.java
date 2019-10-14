7d98b4b6e1df8063b3ab76167947d9c16441a413
Philip Helger <philip@helger.com>
2019-10-14 18:27:15 +0200

    Version update

--- src/test/java/com/helger/peppol/supplementary/tools/MainListVESIDs.java
+++ src/test/java/com/helger/peppol/supplementary/tools/MainListVESIDs.java
@@ -16,25 +16,63 @@
  */
 package com.helger.peppol.supplementary.tools;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import com.helger.bdve.executorset.IValidationExecutorSet;
 import com.helger.bdve.executorset.VESID;
+import com.helger.commons.collection.impl.ICommonsOrderedMap;
+import com.helger.commons.io.file.SimpleFileIO;
 import com.helger.peppol.bdve.ExtValidationKeyRegistry;
 
-public class MainListVESIDs
+public final class MainListVESIDs
 {
+  private static String _getPayload ()
+  {
+    final ICommonsOrderedMap <VESID, IValidationExecutorSet> aAll = ExtValidationKeyRegistry.getAllSortedByID ();
+
+    final StringBuilder aSB = new StringBuilder ();
+    aSB.append ("<!-- ").append (aAll.size ()).append (" entries -->\n");
+    aSB.append ("<ul>\n");
+    for (final Map.Entry <VESID, IValidationExecutorSet> aEntry : aAll.entrySet ())
+    {
+      aSB.append ("<li><code>")
+         .append (aEntry.getKey ().getAsSingleID ())
+         .append ("</code> - ")
+         .append (aEntry.getValue ().getDisplayName ())
+         .append (aEntry.getValue ().isDeprecated () ? " <strong>(Deprecated)</strong>" : "")
+         .append ("</li>\n");
+    }
+    return aSB.append ("</ul>\n").toString ();
+  }
+
   public static void main (final String [] args)
   {
-    for (final Map.Entry <VESID, IValidationExecutorSet> aEntry : ExtValidationKeyRegistry.getAllSortedByID ()
-                                                                                          .entrySet ())
-    {
-      System.out.println ("<li><code>" +
-                          aEntry.getKey ().getAsSingleID () +
-                          "</code> - " +
-                          aEntry.getValue ().getDisplayName () +
-                          (aEntry.getValue ().isDeprecated () ? " <strong>(Deprecated)</strong>" : "") +
-                          "</li>");
-    }
+    final String sNewPayload = _getPayload ();
+
+    final File aFile = new File ("src/main/resources/viewpages/en/validation_dvs.xml");
+    if (!aFile.exists ())
+      throw new IllegalStateException ();
+
+    final String sOld = SimpleFileIO.getFileAsString (aFile, StandardCharsets.UTF_8);
+
+    final String sStart = "<!-- Created by MainListVESIDs -->";
+    final int nStart = sOld.indexOf (sStart);
+    if (nStart < 0)
+      throw new IllegalStateException ();
+
+    final String sEnd = "<!-- end of MainListVESIDs -->";
+    final int nEnd = sOld.indexOf (sEnd);
+    if (nEnd < 0)
+      throw new IllegalStateException ();
+
+    final String sNew = new StringBuilder ().append (sOld, 0, nStart + sStart.length ())
+                                            .append ('\n')
+                                            .append (sNewPayload)
+                                            .append (sOld, nEnd, sOld.length ())
+                                            .toString ();
+    SimpleFileIO.writeFile (aFile, sNew, StandardCharsets.UTF_8);
+    System.out.println ("Finished updating " + aFile.getAbsolutePath ());
   }
 }
