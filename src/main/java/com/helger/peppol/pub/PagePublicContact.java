347c6cd8b3fd65566983e723686eba1e062e17c0
Philip Helger <philip@helger.com>
2021-05-17 11:49:44 +0200

    Ignoring URL

--- src/main/java/com/helger/peppol/pub/PagePublicContact.java
+++ src/main/java/com/helger/peppol/pub/PagePublicContact.java
@@ -104,6 +104,7 @@ public final class PagePublicContact extends AbstractAppWebPage
                                                              "//www.follmex.",
                                                              "//www.godlikeproductions.com",
                                                              "//www.interactivewise.com",
+                                                             "//www.speed-seo.net",
                                                              "//www.talkwithcustomer.com",
                                                              "//www.talkwithwebvisitors.com",
                                                              "//www.targeted-visitors-4yoursite.xyz",
