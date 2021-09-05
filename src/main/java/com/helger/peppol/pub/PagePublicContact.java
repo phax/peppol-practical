96987a4287af7d1acc75d4d2a8dc3f83ea6194e3
Philip Helger <philip@helger.com>
2021-09-05 18:15:48 +0200

    Ignore more

--- src/main/java/com/helger/peppol/pub/PagePublicContact.java
+++ src/main/java/com/helger/peppol/pub/PagePublicContact.java
@@ -68,6 +68,7 @@ public final class PagePublicContact extends AbstractAppWebPage
   // Must only contain lowercase values
   private static final String [] SPAM_KEYS = new String [] { "capshop.online",
                                                              "caredogbest.online",
+                                                             "coolshorts.online",
                                                              "dogloverclub.store",
                                                              "hineck.online",
                                                              "idgod.ch",
