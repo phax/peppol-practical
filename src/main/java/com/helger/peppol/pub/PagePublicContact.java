3bb4847ddadddc94bf41386a57f1de020112c269
Philip Helger <philip@helger.com>
2022-12-23 12:44:11 +0100

    Improved contact page

--- src/main/java/com/helger/peppol/pub/PagePublicContact.java
+++ src/main/java/com/helger/peppol/pub/PagePublicContact.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.debug.GlobalDebug;
 import com.helger.commons.email.EmailAddress;
 import com.helger.commons.email.EmailAddressHelper;
 import com.helger.commons.string.StringHelper;
@@ -33,6 +34,8 @@ import com.helger.html.hc.impl.HCNodeList;
 import com.helger.peppol.app.AppConfig;
 import com.helger.peppol.app.AppHelper;
 import com.helger.peppol.ui.page.AbstractAppWebPage;
+import com.helger.photon.audit.AuditHelper;
+import com.helger.photon.bootstrap4.CBootstrapCSS;
 import com.helger.photon.bootstrap4.button.BootstrapSubmitButton;
 import com.helger.photon.bootstrap4.form.BootstrapForm;
 import com.helger.photon.bootstrap4.form.BootstrapFormGroup;
@@ -57,6 +60,7 @@ public final class PagePublicContact extends AbstractAppWebPage
   private static final String FIELD_NAME = "name";
   private static final String FIELD_EMAIL = "email";
   private static final String FIELD_TOPIC = "reason";
+  private static final String FIELD_HONEYPOT = "title";
   private static final String FIELD_TEXT = "topic";
   private static final String FIELD_CAPTCHA = "captcha";
 
@@ -127,7 +131,8 @@ public final class PagePublicContact extends AbstractAppWebPage
 
     final String sRecaptchWebKey = AppConfig.getRecaptchaWebKey ();
     final String sRecaptchSecretKey = AppConfig.getRecaptchaSecretKey ();
-    final boolean bRecaptchaEnabled = StringHelper.hasText (sRecaptchWebKey) && StringHelper.hasText (sRecaptchSecretKey);
+    final boolean bRecaptchaEnabled = StringHelper.hasText (sRecaptchWebKey) &&
+                                      StringHelper.hasText (sRecaptchSecretKey);
 
     aNodeList.addChild (p ("If you have general questions concerning Peppol technology, you may contact me using the form below. Please be aware, that I run this page on a voluntary basis and that the answers you may receive are my personal answers and not official OpenPeppol answers."));
 
@@ -135,11 +140,12 @@ public final class PagePublicContact extends AbstractAppWebPage
     final FormErrorList aFormErrors = new FormErrorList ();
     if (aWPEC.hasAction (CPageParam.ACTION_PERFORM))
     {
-      final String sName = StringHelper.trim (aWPEC.params ().getAsString (FIELD_NAME));
-      final String sEmail = StringHelper.trim (aWPEC.params ().getAsString (FIELD_EMAIL));
-      final String sTopic = aWPEC.params ().getAsString (FIELD_TOPIC);
-      final String sText = StringHelper.trim (aWPEC.params ().getAsString (FIELD_TEXT));
-      final String sReCaptchaResponse = StringHelper.trim (aWPEC.params ().getAsString (HCReCaptchaV2.RESPONSE_PARAMETER_NAME));
+      final String sName = aWPEC.params ().getAsStringTrimmed (FIELD_NAME);
+      final String sEmail = aWPEC.params ().getAsStringTrimmed (FIELD_EMAIL);
+      final String sTopic = aWPEC.params ().getAsStringTrimmed (FIELD_TOPIC);
+      final String sHoneyPot = aWPEC.params ().getAsString (FIELD_HONEYPOT);
+      final String sText = aWPEC.params ().getAsStringTrimmed (FIELD_TEXT);
+      final String sReCaptchaResponse = aWPEC.params ().getAsStringTrimmed (HCReCaptchaV2.RESPONSE_PARAMETER_NAME);
 
       if (StringHelper.hasNoText (sName))
         aFormErrors.addFieldError (FIELD_NAME, "Your name must be provided.");
@@ -166,27 +172,46 @@ public final class PagePublicContact extends AbstractAppWebPage
 
       if (aFormErrors.isEmpty ())
       {
-        if (!_isSpamBody (sText))
+        if (StringHelper.hasText (sHoneyPot))
         {
-          final EmailData aEmailData = new EmailData (EEmailType.TEXT);
-          aEmailData.setFrom (new EmailAddress ("peppol-practical@helger.com", "Peppol Practical"));
-          aEmailData.to ().add (new EmailAddress ("philip@helger.com"));
-          aEmailData.replyTo ().add (new EmailAddress (sEmail, sName));
-          aEmailData.setSubject ("[" + AppHelper.getApplicationTitle () + "] Contact Form - " + sName);
-
-          final StringBuilder aSB = new StringBuilder ();
-          aSB.append ("Contact form from " + AppHelper.getApplicationTitle () + " was filled out.\n\n");
-          aSB.append ("Name: ").append (sName).append ("\n");
-          aSB.append ("Email: ").append (sEmail).append ("\n");
-          aSB.append ("Topic: ").append (sTopic).append ("\n");
-          aSB.append ("Text:\n").append (sText).append ("\n");
-          aEmailData.setBody (aSB.toString ());
-
-          LOGGER.info ("Sending contact form from '" + sName + "' and email '" + sEmail + "' with topic '" + sTopic + "'");
-          ScopedMailAPI.getInstance ().queueMail (InternalErrorSettings.getSMTPSettings (), aEmailData);
+          LOGGER.warn ("The Honeypot field was filled out - not sending an email from the contact page");
+          if (GlobalDebug.isDebugMode ())
+            aNodeList.addChild (h2 ("You trapped into the Honeypot"));
+          AuditHelper.onAuditExecuteSuccess ("contact-spam-protection", sEmail);
         }
         else
-          LOGGER.info ("Ignoring spam contact form from '" + sEmail + "' with topic '" + sTopic + "'");
+          if (_isSpamBody (sText))
+          {
+            LOGGER.warn ("Ignoring spam contact form from '" + sEmail + "' with topic '" + sTopic + "'");
+            if (GlobalDebug.isDebugMode ())
+              aNodeList.addChild (h2 ("You are trying to send Spam"));
+            AuditHelper.onAuditExecuteSuccess ("contact-spam-protection", sEmail, sTopic);
+          }
+          else
+          {
+            final EmailData aEmailData = new EmailData (EEmailType.TEXT);
+            aEmailData.setFrom (new EmailAddress ("peppol-practical@helger.com", "Peppol Practical"));
+            aEmailData.to ().add (new EmailAddress ("philip@helger.com"));
+            aEmailData.replyTo ().add (new EmailAddress (sEmail, sName));
+            aEmailData.setSubject ("[" + AppHelper.getApplicationTitle () + "] Contact Form - " + sName);
+
+            final StringBuilder aSB = new StringBuilder ();
+            aSB.append ("Contact form from " + AppHelper.getApplicationTitle () + " was filled out.\n\n");
+            aSB.append ("Name: ").append (sName).append ("\n");
+            aSB.append ("Email: ").append (sEmail).append ("\n");
+            aSB.append ("Topic: ").append (sTopic).append ("\n");
+            aSB.append ("Text:\n").append (sText).append ("\n");
+            aEmailData.setBody (aSB.toString ());
+
+            LOGGER.info ("Sending contact form from '" +
+                         sName +
+                         "' and email '" +
+                         sEmail +
+                         "' with topic '" +
+                         sTopic +
+                         "'");
+            ScopedMailAPI.getInstance ().queueMail (InternalErrorSettings.getSMTPSettings (), aEmailData);
+          }
 
         aNodeList.addChild (success ("Thank you for your message. Please note that I run this page on a voluntary basis on my expenses - you may consider a donation."));
         bShowForm = false;
@@ -207,14 +232,24 @@ public final class PagePublicContact extends AbstractAppWebPage
       aSelect.addOption ("SML/SMK");
       aSelect.addOption ("SMP");
       aSelect.addOption ("AccessPoint (AP)/AS4");
-      aSelect.addOption ("Peppol Directory");
       aSelect.addOption ("Validation");
+      aSelect.addOption ("Peppol Directory");
       aSelect.addOption ("CEF");
       aSelect.addOption ("General question");
       aSelect.addOptionPleaseSelect (aDisplayLocale);
       aForm.addFormGroup (new BootstrapFormGroup ().setLabel ("Topic")
                                                    .setCtrl (aSelect)
                                                    .setErrorList (aFormErrors.getListOfField (FIELD_TOPIC)));
+
+      {
+        // Honeypot field
+        // If this field is filled out, the filler is a bot
+        final BootstrapFormGroup aBG = new BootstrapFormGroup ().setLabelMandatory ("Message title")
+                                                                .setCtrl (new HCEdit (new RequestField (FIELD_HONEYPOT)))
+                                                                .setErrorList (aFormErrors.getListOfField (FIELD_HONEYPOT));
+        aBG.cssClasses ().addClass (CBootstrapCSS.D_NONE);
+        aForm.addFormGroup (aBG);
+      }
 
       aForm.addFormGroup (new BootstrapFormGroup ().setLabelMandatory ("Your message")
                                                    .setCtrl (new HCTextAreaAutosize (new RequestField (FIELD_TEXT)).setRows (5))
