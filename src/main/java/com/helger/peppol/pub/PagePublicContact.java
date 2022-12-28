b18f83824c3d570c68d9ee655a7f4b092ee4de7c
Philip Helger <philip@helger.com>
2022-12-28 09:55:03 +0100

    More spam domains

--- src/main/java/com/helger/peppol/pub/PagePublicContact.java
+++ src/main/java/com/helger/peppol/pub/PagePublicContact.java
@@ -67,6 +67,7 @@ public final class PagePublicContact extends AbstractAppWebPage
 
   // Must only contain lowercase values
   private static final String [] SPAM_KEYS = new String [] { "Get Yours Here:",
+                                                             "boostleadgeneration.com",
                                                              "dogloverclub.store",
                                                              "idgod.ch",
                                                              "magicmat.shop",
@@ -127,7 +128,8 @@ public final class PagePublicContact extends AbstractAppWebPage
 
     final String sRecaptchWebKey = AppConfig.getRecaptchaWebKey ();
     final String sRecaptchSecretKey = AppConfig.getRecaptchaSecretKey ();
-    final boolean bRecaptchaEnabled = StringHelper.hasText (sRecaptchWebKey) && StringHelper.hasText (sRecaptchSecretKey);
+    final boolean bRecaptchaEnabled = StringHelper.hasText (sRecaptchWebKey) &&
+                                      StringHelper.hasText (sRecaptchSecretKey);
 
     aNodeList.addChild (p ("If you have general questions concerning Peppol technology, you may contact me using the form below. Please be aware, that I run this page on a voluntary basis and that the answers you may receive are my personal answers and not official OpenPeppol answers."));
 
@@ -139,7 +141,8 @@ public final class PagePublicContact extends AbstractAppWebPage
       final String sEmail = StringHelper.trim (aWPEC.params ().getAsString (FIELD_EMAIL));
       final String sTopic = aWPEC.params ().getAsString (FIELD_TOPIC);
       final String sText = StringHelper.trim (aWPEC.params ().getAsString (FIELD_TEXT));
-      final String sReCaptchaResponse = StringHelper.trim (aWPEC.params ().getAsString (HCReCaptchaV2.RESPONSE_PARAMETER_NAME));
+      final String sReCaptchaResponse = StringHelper.trim (aWPEC.params ()
+                                                                .getAsString (HCReCaptchaV2.RESPONSE_PARAMETER_NAME));
 
       if (StringHelper.hasNoText (sName))
         aFormErrors.addFieldError (FIELD_NAME, "Your name must be provided.");
@@ -182,7 +185,13 @@ public final class PagePublicContact extends AbstractAppWebPage
           aSB.append ("Text:\n").append (sText).append ("\n");
           aEmailData.setBody (aSB.toString ());
 
-          LOGGER.info ("Sending contact form from '" + sName + "' and email '" + sEmail + "' with topic '" + sTopic + "'");
+          LOGGER.info ("Sending contact form from '" +
+                       sName +
+                       "' and email '" +
+                       sEmail +
+                       "' with topic '" +
+                       sTopic +
+                       "'");
           ScopedMailAPI.getInstance ().queueMail (InternalErrorSettings.getSMTPSettings (), aEmailData);
         }
         else
