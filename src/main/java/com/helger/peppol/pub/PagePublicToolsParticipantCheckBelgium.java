99e123757b8d6a67196ec6630fc65f5a5b0ad803
Philip Helger <philip@helger.com>
2025-01-17 21:17:49 +0100

    Fixed potential NPE

--- src/main/java/com/helger/peppol/pub/PagePublicToolsParticipantCheckBelgium.java
+++ src/main/java/com/helger/peppol/pub/PagePublicToolsParticipantCheckBelgium.java
@@ -72,13 +72,13 @@ public class PagePublicToolsParticipantCheckBelgium extends AbstractAppWebPage
                                                                          PeppolIdentifierHelper.DEFAULT_PARTICIPANT_SCHEME,
                                                                          "0208:" + sParticipantIDValue,
                                                                          false);
-    final boolean bIsCBE = aSMPQP_CBE.isSMPRegisteredInDNS ();
+    final boolean bIsCBE = aSMPQP_CBE == null ? false : aSMPQP_CBE.isSMPRegisteredInDNS ();
 
     final SMPQueryParams aSMPQP_VAT = SMPQueryParams.createForSMLOrNull (aSMLConfiguration,
                                                                          PeppolIdentifierHelper.DEFAULT_PARTICIPANT_SCHEME,
                                                                          "9925:" + sParticipantIDValue,
                                                                          false);
-    final boolean bIsVAT = aSMPQP_VAT.isSMPRegisteredInDNS ();
+    final boolean bIsVAT = aSMPQP_VAT == null ? false : aSMPQP_VAT.isSMPRegisteredInDNS ();
     if (bIsCBE || bIsVAT)
     {
       if (bIsCBE)
