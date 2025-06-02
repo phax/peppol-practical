2982c0b7ed1b239c3efffeb43f44160a8980408a
Philip Helger <philip@helger.com>
2025-06-02 23:19:27 +0200

    Version bump

--- src/main/java/com/helger/peppol/rest/APISMPQueryGetServiceInformation.java
+++ src/main/java/com/helger/peppol/rest/APISMPQueryGetServiceInformation.java
@@ -44,14 +44,11 @@ import com.helger.peppol.sharedui.mgr.SharedUIMetaManager;
 import com.helger.peppolid.IDocumentTypeIdentifier;
 import com.helger.peppolid.IParticipantIdentifier;
 import com.helger.peppolid.factory.SimpleIdentifierFactory;
-import com.helger.peppolid.peppol.PeppolIdentifierHelper;
-import com.helger.peppolid.peppol.Pfuoi430;
 import com.helger.photon.api.IAPIDescriptor;
 import com.helger.servlet.response.UnifiedResponse;
 import com.helger.smpclient.bdxr1.BDXRClientReadOnly;
 import com.helger.smpclient.bdxr2.BDXR2ClientReadOnly;
 import com.helger.smpclient.json.SMPJsonResponse;
-import com.helger.smpclient.peppol.PeppolWildcardSelector.EMode;
 import com.helger.smpclient.peppol.SMPClientReadOnly;
 import com.helger.web.scope.IRequestWebScopeWithoutResponse;
 
@@ -62,7 +59,6 @@ public final class APISMPQueryGetServiceInformation extends AbstractPPAPIExecuto
 
   private static final Logger LOGGER = LoggerFactory.getLogger (APISMPQueryGetServiceInformation.class);
 
-  @SuppressWarnings ("removal")
   @Override
   protected void rateLimitedInvokeAPI (@Nonnull final IAPIDescriptor aAPIDescriptor,
                                        @Nonnull @Nonempty final String sPath,
@@ -159,19 +155,8 @@ public final class APISMPQueryGetServiceInformation extends AbstractPPAPIExecuto
         aSMPClient.setVerifySignature (bVerifySignature);
 
         final com.helger.xsds.peppol.smp1.SignedServiceMetadataType aSSM;
-        if (PDTFactory.getCurrentZonedDateTimeUTC ().toLocalDate ().compareTo (Pfuoi430.VALID_FROM) >= 0)
-        {
-          // PFUOI 4.3.0
-          aSSM = aSMPClient.getSchemeSpecificServiceMetadataOrNull (aParticipantID, aDocTypeID);
-        }
-        else
-        {
-          // PFUOI 4.2.0
-          if (PeppolIdentifierHelper.DOCUMENT_TYPE_SCHEME_PEPPOL_DOCTYPE_WILDCARD.equals (aDocTypeID.getScheme ()))
-            aSSM = aSMPClient.getWildcardServiceMetadataOrNull (aParticipantID, aDocTypeID, EMode.BUSDOX_THEN_WILDCARD);
-          else
-            aSSM = aSMPClient.getServiceMetadataOrNull (aParticipantID, aDocTypeID);
-        }
+        // PFUOI 4.3.0
+        aSSM = aSMPClient.getSchemeSpecificServiceMetadataOrNull (aParticipantID, aDocTypeID);
         if (aSSM != null)
         {
           final com.helger.xsds.peppol.smp1.ServiceMetadataType aSM = aSSM.getServiceMetadata ();
